b9e456e18d07fa4aef61e1765ee533218d7886f8
KarlGalinski <52929923+KarlGalinski@users.noreply.github.com>
2019-07-29 16:46:26 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,24 @@
+import {Book} from "../../app/bookInfo";
+export const MYBOOKS: Book[] = 
+[
+    {bookName:"Harry Potter",
+    author:"J. K. Rowling",
+    bookGenre:"Fantasy",
+    yearPublished:"2005",
+    picture:"assets/images/HarryPotter.jpg"},
+    {bookName:"Spider-Man",
+    author:"Stan Lee",
+    bookGenre:"Comic",
+    yearPublished:"1962",
+    picture:"assets/images/SpiderMan.png"},
+    {bookName:"IT",
+    author:"Stephen King",
+    bookGenre:"Horror",
+    yearPublished:"1986",
+    picture:"assets/images/It.jpg"},
+    {bookName:"Mody-Dick",
+    author:"Herman Melville",
+    bookGenre:"Novel",
+    yearPublished:"1851",
+    picture:"assets/images/MobyDick.jpg"}
+]
